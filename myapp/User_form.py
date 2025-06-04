a15db987a82be31f935109442b01f5c29b15873d
mahomuda_mim <mahomudamim@gmail.com>
2025-06-05 02:25:43 +0600

    Last update with testing code

--- myapp/User_form.py
+++ myapp/User_form.py
@@ -4,57 +4,27 @@ from .models import *
 
 class LoginForm(forms.Form):
     username = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                'class':
-                    'form-control'
-            }
-        )
+        widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'username'})
     )
     password = forms.CharField(
-        widget=forms.PasswordInput(
-            attrs={
-                'class':
-                    'form-control'
-            }
-        )
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'id': 'password'})
     )
 ####################################################################
 class SignupForm(UserCreationForm):
-   username = forms.CharField(
-       widget=forms.TextInput(
-           attrs={
-               'class':
-                   'form-control'
-           }
-       )
-   )
-   password1 = forms.CharField(
-       widget=forms.PasswordInput(
-           attrs={
-               'class':
-                   'form-control'
-           }
-       )
-   )
-   password2 = forms.CharField(
-       widget=forms.PasswordInput(
-           attrs={
-               'class':
-                   'form-control'
-           }
-       )
-   )
-   email = forms.CharField(
-       widget=forms.TextInput(
-           attrs={
-               'class':
-                   'form-control'
-           }
-       )
-   )
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'username'})
+    )
+    email = forms.CharField(
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'id': 'email'})
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'id': 'password1'})
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'id': 'password2'})
+    )
 
-   class Meta:
+    class Meta:
        model = User
        fields = ['username', 'email', 'password1', 'password2','user_type']
 
